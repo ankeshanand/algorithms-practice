b3ea75753910b2665410660c836f40f137b83988
Ankesh Anand <ankeshanand1994@gmail.com>
2013-12-01 23:13:06 +0530

    Create string-to-int.cpp

---
+++ string-to-int.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<cstdlib>
+#include<cstdio>
+using namespace std;
+int main()
+{
+    int i;
+    string str;
+    
+    /*Method 1 - Convert string to Int using sprintf (Fast) */
+    char buffer[10];
+    sprintf(buffer,"%d",i);
+    str = string(buffer);
+    cout << str << endl;
+    
+    /*Method 2 - Convert string to Int using stringstream (Slow) */
+    stringstream ss;
+    ss << i;
+    str = ss.str();
+    cout << str << endl;
+}
